5f74ed7fadde526b3c69e7ffab8af879eec1b471
lebahai1608 <hai110451@donga.edu.vn>
2024-11-05 10:29:52 +0700

    Add files via upload

---
+++ THỰC HÀNH/BUỔI 4/Hai110451.java
@@ -0,0 +1,17 @@
+Public class Hai110451 {
+public static void main(String[] args) {
+    scanner sc = new Scanner(System.in);
+    int n;
+    white(true)
+    {
+        try {
+            System.out.print(s:"\nNhập gái trị: ");
+            n = Intoger.parseInt(sc.nextLine());
+            break;
+
+        } catch (Exception ex) {
+            System.out.println(x:"Dữ liệu không hợp lệ, hãy nhập lại !!!");
+        }
+    }
+}
+HienThiGiaTri(n);
