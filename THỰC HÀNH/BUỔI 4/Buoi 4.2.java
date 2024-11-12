cf93aa0e9f331b50fd88e7125339c94a666ef78a
lebahai1608 <hai110451@donga.edu.vn>
2024-11-12 12:20:27 +0700

    Add files via upload

---
+++ THỰC HÀNH/BUỔI 4/Buoi 4.2.java
@@ -0,0 +1,16 @@
+package buoi4;
+
+public class Yen108652 {
+    public static void vidu5() {
+        String nguHanh[] = { "kim", "moc", "thuy", "hoa", "tho" };
+        System.out.print("Danh sách các phần tử của mảng là: ");
+        for (int i = 0; i < nguHanh.length; i++) {
+            System.out.printf("\n nguHanh[%d] = %S",i, nguHanh[i]);
+           
+        }
+
+    }
+    public static void main(String[] args) {
+        vidu5();
+    }
+}
